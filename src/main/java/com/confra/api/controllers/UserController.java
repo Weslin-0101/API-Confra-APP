e71507dfa65b202ce38c60eb2985410a4883bc26
Wesley Lira <wesleeylirac12@gmail.com>
2023-08-09 10:01:30 -0300

    feat: added user controllers, repositories and service

---
+++ src/main/java/com/confra/api/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.confra.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(
+        value = "root/confra/api/v1"
+)
+public class UserController {
+}
---
+++ src/main/java/com/confra/api/service/UserService.java
@@ -0,0 +1,7 @@
+package com.confra.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
