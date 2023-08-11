0ed0dbeacd7f455bbea3b9360f6d524b3a6cea7d
Wesley Lira <wesleeylirac12@gmail.com>
2023-08-10 22:16:19 -0300

    feat: added secretKey, jwtExpiration and refreshToken into application.yml

--- src/main/java/com/confra/api/authorizationJwt/JwtService.java
+++ src/main/java/com/confra/api/authorizationJwt/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,12 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "jM+zON+rwRL8KV63UOB3+pJOEw8O2/SamvujhimQtWRhW1kqoq37fKu4QXcE5leKLUUk+7UmPSAIW30QIOg5ktkmCYg1s/wh0SNr7FV+sle40ql6H2xnR+NgaX+T15a61cBAgNK9Pgtly2fuy/XYwmUIB/Zur86Lo+V03z2FMxCtcbDJEovW2sWaEVoKXz5GOonvvFRvv73NGmO43uDRYgJx5Mbb2Q/8c5+ayyiDtYje+2hBOZFbwkxvQJocbERN9W9LwfBd+G+VbqBCFx9TybuGJQ272Z2STgNKvwDiQPLTqH2eAX6xKysawTMuHvpDLBhITKnUROWfYixjMT/MFX1VfZWZeBygDImnY0ykxoPFUsQe3wtweWmpa+j2rN/p9KySzu5/iZdcuLVz0iYu1OAnmpfiULYn3Q3pPaMG0VeApI+0hxFnVxB+kUAI4Y1WLUVCuLEESCPP9caAFYEG1sN1NBPZ50EwA9Q+oZiYn+83p53uey1GLFaLWKM2A5VSiUBJWADW8anqz5o8Ov5n9p/7YywdLiL2J5W7KJy7RvkF06igRIvpvWTlUc8DriO3ZuAstpM8x1Ch+CD3a6zlUN1HGw+Y6dM1fVJ890+9bC8mUTCGYgR3OmbSwU61e/99R/xIjqt2v2fiKyKVoUnCqlSSwIyFTa8e5A6dCQPIUUc=\n";
+    @Value("${application.security.jwt.secret-key}")
+    private String SECRET_KEY;
+    @Value("{application.security.jwt.expiration}")
+    private Long jwtExpiration;
+    @Value("{application.security.jwt.refresh-token.expiration}")
+    private Long refreshExpiration;
 
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -28,7 +34,7 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    public String generateTokenWithoutClaims(UserDetails userDetails) {
+    public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
     }
 
@@ -36,12 +42,24 @@ public class JwtService {
             Map<String, Object> extractClaims,
             UserDetails userDetails
     ) {
+        return buildToken(extractClaims, userDetails, jwtExpiration);
+    }
+
+    public String generateRefreshToken(UserDetails userDetails) {
+        return buildToken(new HashMap<>(), userDetails, refreshExpiration);
+    }
+
+    private String buildToken(
+            Map<String, Object> extractClaims,
+            UserDetails userDetails,
+            Long expiration
+    ) {
         return Jwts
                 .builder()
                 .setClaims(extractClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
