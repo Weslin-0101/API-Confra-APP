44ddd139758ca19a0724447f44ae0d09857a8cf5
Rafael M. Ramos <rafaelmrdsilva@gmail.com>
2023-08-29 10:41:49 -0300

    teste do commit

--- src/main/java/com/confra/api/controllers/UserController.java
+++ src/main/java/com/confra/api/controllers/UserController.java
@@ -1,12 +1,23 @@
 package com.confra.api.controllers;
 
+import com.confra.api.model.User;
 import com.confra.api.model.dto.UserDTO.RegisterRequest;
 import com.confra.api.model.dto.UserDTO.RegisterResponse;
 import com.confra.api.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(
@@ -16,14 +27,50 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
-
-    @PostMapping("/register")
-    public ResponseEntity<RegisterResponse> create (@RequestBody @Valid RegisterRequest request) throws Exception {
-        return ResponseEntity.ok(userService.createAccount(request));
+    @PostMapping("/create")
+    public ResponseEntity<RegisterResponse> saveProduct(@RequestBody @Valid RegisterRequest registerRequest) {
+        var user = new User();
+        BeanUtils.copyProperties(registerRequest, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createAccount(registerRequest));
+    }
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> userList = userService.findAll();
+        if (!userList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(userList);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userList);
     }
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> get() {
-        return ResponseEntity.ok("Hello World");
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getOneUser(@PathVariable(value="id") UUID id){
+        var user = userService.findById(id);
+        if (user == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+//      user.add(linkTo(methodOn(UserController.class).getAllUsers()).withRel("User List"));
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PutMapping("/put/{id}")
+    public ResponseEntity<User> updateUser (@PathVariable(value = "id") UUID id,
+                                            @RequestBody @Valid RegisterRequest registerRequest){
+        Optional<User> userObject = Optional.ofNullable(userService.findById(id));
+        if (userObject.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        var user = userObject.get();
+        BeanUtils.copyProperties(registerRequest, user);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.save(user));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<User> deleteUser (@PathVariable(value = "id") UUID id){
+        Optional<User> userObject = Optional.ofNullable(userService.findById(id));
+        if (userObject.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        userService.delete(userObject.get());
+        return ResponseEntity.status(HttpStatus.OK).body(userObject.get());
     }
 }
