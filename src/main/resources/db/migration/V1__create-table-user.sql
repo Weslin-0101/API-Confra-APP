fb9cf7a95c26569cc9f1a292c2a19559624c07c3
weslinn <wesleeylirac12@gmail.com>
2023-11-02 17:52:50 -0300

    refactor: refactoring the dt_resgistration and added email unique

--- src/main/resources/db/migration/V1__create-table-user.sql
+++ src/main/resources/db/migration/V1__create-table-user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public._user (
     id uuid NOT NULL,
     cod_document VARCHAR(11) NOT NULL,
-    dt_registration TIMESTAMP NOT NULL,
+    dt_registration DATE NOT NULL,
     desc_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
@@ -12,5 +12,6 @@ CREATE TABLE IF NOT EXISTS public._user (
     role VARCHAR(20) NOT NULL,
 
     CONSTRAINT pk_user PRIMARY KEY (id),
+    CONSTRAINT unique_email UNIQUE (email),
     CONSTRAINT "_user_role_check" CHECK (((role)::text = ANY ((ARRAY['USER'::character varying, 'ADMIN'::character varying])::text[])))
 );
