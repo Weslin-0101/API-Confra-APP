0d46ecf52821bb3c02071de3052d35d90c03164b
Wesley Lira <wesleeylirac12@gmail.com>
2024-01-01 12:42:26 -0300

    test: should return an user by email calling findUserByEmailGateway

--- src/test/java/com/confra/api/application/useCases/FindUserByEmailInteractorTest.java
+++ src/test/java/com/confra/api/application/useCases/FindUserByEmailInteractorTest.java
@@ -1,27 +1,41 @@
 package com.confra.api.application.useCases;
 
 import com.confra.api.application.gateways.UserFindByEmailGateway;
-import com.confra.api.application.gateways.UserGateway;
 import com.confra.api.domain.UserEntity;
 import com.confra.api.mocks.MockUser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class FindUserByEmailInteractorTest {
+    @InjectMocks
+    private FindUserByEmailInteractor sut;
+    @Mock
+    private UserFindByEmailGateway userGateway;
+    private MockUser mockUser;
+
+    @BeforeEach
+    void setUp() {
+        mockUser = new MockUser();
+    }
 
     @Test
     void shouldReturnAnUserByEmail() {
-        FindUserByEmailInteractor sut = new FindUserByEmailInteractor();
+        var request = mockUser.mockUserEntity();
+        UserEntity expectedResult = mockUser.mockUserEntity();
 
-        MockUser mock = new MockUser();
-        var request = mock.mockUserEntity();
-        UserEntity expectedResult = mock.mockUserEntity();
+        Mockito.when(userGateway.findUserByEmail(request.getEmail())).thenReturn(expectedResult);
 
         UserEntity result = sut.findUserByEmail(request.getEmail());
 
         assertNotNull(result);
-        assertEquals(expectedResult.getEmail(), result.getEmail());
+        assertEquals(expectedResult.getEmail(), request.getEmail());
     }
 }
