6320b55f65337c82d89579fc3f51c2a835201c38
Wesley Lira <wesleeylirac12@gmail.com>
2024-01-01 14:58:27 -0300

    test: should call DeleteUserByEmailGateway

---
+++ src/test/java/com/confra/api/application/useCases/DeleteUserByEmailInteractorTest.java
@@ -0,0 +1,36 @@
+package com.confra.api.application.useCases;
+
+import com.confra.api.application.gateways.DeleteUserByEmailGateway;
+import com.confra.api.domain.UserEntity;
+import com.confra.api.mocks.MockUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteUserByEmailInteractorTest {
+    @InjectMocks
+    private DeleteUserByEmailInteractor sut;
+    @Mock
+    private DeleteUserByEmailGateway deleteUserByEmailGateway;
+    private MockUser user = new MockUser();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        user = new MockUser();
+    }
+
+    @Test
+    void shouldCallDeleteAnUserByEmailGateway() {
+        var request = user.mockUserEntity();
+
+        sut.deleteUserByEmail(request.getEmail());
+
+        Mockito.verify(deleteUserByEmailGateway).deleteUserByEmail(request.getEmail());
+    }
+}
