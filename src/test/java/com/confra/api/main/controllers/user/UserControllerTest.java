de93de9f7b0853bafc766a8618f922d9e1b5dc60
Wesley Lira <wesleeylirac12@gmail.com>
2024-01-02 19:15:41 -0300

    refactor: adding MockBean annotation

--- src/test/java/com/confra/api/main/controllers/user/UserControllerTest.java
+++ src/test/java/com/confra/api/main/controllers/user/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.confra.api.main.controllers.user;
 
 import com.confra.api.application.useCases.CreateUserInteractor;
 import com.confra.api.domain.UserEntity;
+import com.confra.api.main.config.authentication.JwtAuthenticationFilter;
 import com.confra.api.main.controllers.dtos.user.RegisterRequestDTO;
 import com.confra.api.main.controllers.dtos.user.RegisterResponseDTO;
 import com.confra.api.main.controllers.dtos.user.UserDTOMapper;
@@ -13,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,12 +30,12 @@ class UserControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
-    @Mock
+    @MockBean
     private CreateUserInteractor createUserUsecase;
-    @Mock
+    @MockBean
     private UserDTOMapper userDTOMapper;
-    @InjectMocks
-    private UserController userController;
+    @MockBean
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Test
     void shouldRequestUserCreation() throws Exception {
