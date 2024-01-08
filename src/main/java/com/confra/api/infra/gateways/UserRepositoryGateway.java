4fb99388e405d8bbcefbcfbdbf6b36ae63e0c9e0
Wesley Lira <wesleeylirac12@gmail.com>
2024-01-08 20:56:28 -0300

    feat: should UserRepositoryGateway throw if email already exists

--- src/main/java/com/confra/api/infra/gateways/UserRepositoryGateway.java
+++ src/main/java/com/confra/api/infra/gateways/UserRepositoryGateway.java
@@ -2,9 +2,12 @@ package com.confra.api.infra.gateways;
 
 import com.confra.api.application.gateways.UserGateway;
 import com.confra.api.domain.UserEntity;
+import com.confra.api.exceptions.RequestNotAllowedException;
 import com.confra.api.exceptions.RequiredObjectsIsNullException;
 import com.confra.api.infra.persistence.repositories.UserRepository;
 import com.confra.api.infra.persistence.tables.User;
+
+import java.util.Optional;
 
 public class UserRepositoryGateway implements UserGateway {
     private final UserRepository userRepository;
@@ -19,6 +22,11 @@ public class UserRepositoryGateway implements UserGateway {
     public UserEntity createUser(UserEntity userDomain) {
         if (userDomain == null) {
             throw new RequiredObjectsIsNullException("User cannot be null");
+        }
+
+        Optional<User> findUser = userRepository.findByEmail(userDomain.getEmail());
+        if (findUser.isPresent()) {
+            throw new RequestNotAllowedException("User already exists");
         }
 
         User userPersistence = userEntityMapper.toEntity(userDomain);
