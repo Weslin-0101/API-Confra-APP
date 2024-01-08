68c83737db74cddc2d00719e3415667f3743569d
Wesley Lira <wesleeylirac12@gmail.com>
2024-01-08 20:03:24 -0300

    test: should UserRepositoryGateway returns an user

---
+++ src/test/java/com/confra/api/infra/gateways/UserRepositoryGatewayTest.java
@@ -0,0 +1,53 @@
+package com.confra.api.infra.gateways;
+
+import com.confra.api.domain.UserEntity;
+import com.confra.api.infra.persistence.repositories.UserRepository;
+import com.confra.api.infra.persistence.tables.User;
+import com.confra.api.mocks.MockUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryGatewayTest {
+
+    @InjectMocks
+    private UserRepositoryGateway sut;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserEntityMapper userEntityMapper;
+    private MockUser mockUser;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockUser = new MockUser();
+    }
+
+    @Test
+    public void shouldCreateAnUser() {
+        UserEntity request = mockUser.mockUserEntity();
+
+        User userPersistence = userEntityMapper.toEntity(request);
+
+        Mockito.when(userRepository.save(userPersistence)).thenReturn(userPersistence);
+        Mockito.when(userEntityMapper.toDomainObject(userPersistence)).thenReturn(request);
+
+        UserEntity result = sut.createUser(request);
+
+        assertNotNull(result);
+        assertEquals(request.getCpf(), result.getCpf());
+        assertEquals(request.getDtRegistration(), result.getDtRegistration());
+        assertEquals(request.getName(), result.getName());
+        assertEquals(request.getLastname(), result.getLastname());
+        assertEquals(request.getEmail(), result.getEmail());
+        assertEquals(request.getPassword(), result.getPassword());
+        assertEquals(request.getTotalInstallments(), result.getTotalInstallments());
+        assertEquals(request.getTotalInstallmentsPaid(), result.getTotalInstallmentsPaid());
+    }
+}
