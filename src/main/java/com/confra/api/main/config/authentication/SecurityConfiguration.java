7554d854782636120df3642ace129c2545691114
Wesley Lira <wesleeylirac12@gmail.com>
2024-01-08 21:38:24 -0300

    refactor: refactoring find user by email endpoint

--- src/main/java/com/confra/api/main/config/authentication/SecurityConfiguration.java
+++ src/main/java/com/confra/api/main/config/authentication/SecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SecurityConfiguration {
                             .requestMatchers(mvc.pattern("confra/api/v1/user/create")).permitAll()
                             .requestMatchers(mvc.pattern("root/confra/api/v1/user/create-admin")).hasRole("ADMIN")
                             .requestMatchers(mvc.pattern("root/confra/api/v1/user")).hasRole("ADMIN")
-                            .requestMatchers(mvc.pattern("root/confra/api/v1/user/{id}")).hasRole("ADMIN")
+                            .requestMatchers(mvc.pattern("confra/api/v1/user/{email}")).permitAll()
                             .requestMatchers(mvc.pattern("root/confra/api/v1/user/{email}")).hasRole("USER")
                             .requestMatchers(mvc.pattern("root/confra/api/v1/user/{id}")).hasRole("ADMIN")
                             .requestMatchers(mvc.pattern("root/confra/api/v1/user/sort")).hasRole("ADMIN")
