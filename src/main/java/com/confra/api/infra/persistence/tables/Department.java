3d37b61d83a986418486352f7ad8b4ce35a2574b
Wesley Lira <wesleeylirac12@gmail.com>
2024-01-09 23:12:30 -0300

    feat: created Department Persistence table

---
+++ src/main/java/com/confra/api/infra/persistence/tables/Department.java
@@ -0,0 +1,43 @@
+package com.confra.api.infra.persistence.tables;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+import java.util.Date;
+
+@Builder
+@Entity
+@Table(name = "_department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idDepartment;
+    @Column(length = 100, unique = true, nullable = false)
+    private String name;
+    @Column(length = 100, nullable = false)
+    private String description;
+    @Column(length = 50, nullable = false)
+    private String supervisor;
+    private Date dtRegistration;
+    private Date dtUpdate;
+
+    public Department(
+        Long idDepartment,
+        String name,
+        String description,
+        String supervisor,
+        Date dtRegistration,
+        Date dtUpdate
+    ) {
+        this.idDepartment = idDepartment;
+        this.name = name;
+        this.description = description;
+        this.supervisor = supervisor;
+        this.dtRegistration = dtRegistration;
+        this.dtUpdate = dtUpdate;
+    }
+
+    public Department() {
+
+    }
+}
